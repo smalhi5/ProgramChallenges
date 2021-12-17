e37b5d483f3d4eee8b893ef7ecf533882f0f56b8
Simran Malhi <simmalhi123@gmail.com>
2021-12-17 15:47:03 -0600

    find numbers that have odd number of zeroes

--- ProgramChallenges/Program.cs
+++ ProgramChallenges/Program.cs
@@ -7,6 +7,31 @@ namespace ProgramChallenges
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[] numbers = { 30, 4, 700, 10070, 3000 };
+            int numCount = FindNumberWithOddNumberOfZero(numbers);
+            Console.WriteLine(numCount);
+        }
+
+        public static int FindNumberWithOddNumberOfZero(int[] numbers) 
+        {
+            int numCount = 0;
+            foreach (int num in numbers)
+            {
+                string tempNum = num.ToString();
+                int zeroCount = 0;
+                for (int i = 0; i < tempNum.Length; i++)
+                {
+                    if (tempNum[i] == '0')
+                    {
+                        zeroCount++;
+                    }
+                }
+                if (zeroCount % 2 == 1) 
+                {
+                    numCount++;
+                }
+            }
+            return numCount;
         }
     }
 }
