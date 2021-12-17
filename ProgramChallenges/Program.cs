9eb90e4409f2dd23060fdb58597965a22607c7a5
Simran Malhi <simmalhi123@gmail.com>
2021-12-17 16:26:04 -0600

    Determind debugger line location

--- ProgramChallenges/Program.cs
+++ ProgramChallenges/Program.cs
@@ -7,9 +7,16 @@ namespace ProgramChallenges
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
             int[] numbers = { 30, 4, 700, 10070, 3000 };
             int numCount = FindNumberWithOddNumberOfZero(numbers);
-            Console.WriteLine(numCount);
+            Console.WriteLine("Number that contain odd number of zeroes" + numCount);
+
+            char[] actions = { 'n', 'c', 'n', 'c', 'c', 'c', 'c' , 'n'};
+            int[] breakpoints = { 5, 10 };
+            int length = 25;
+            int currentLine = DetermindTheLineDuringDebugger(actions, breakpoints, length);
+            Console.WriteLine("Current Line the debugger " + currentLine);
         }
 
         public static int FindNumberWithOddNumberOfZero(int[] numbers) 
@@ -32,6 +39,40 @@ namespace ProgramChallenges
                 }
             }
             return numCount;
+        }
+
+        public static int DetermindTheLineDuringDebugger(char[] actions, int[] breakpoint, int length)
+        {
+            int currentLine = 1;
+            foreach (char action in actions) 
+            {
+                if (action == 'n' && currentLine < length)
+                {
+                    currentLine++;
+                }
+                if (action == 'c' && currentLine < length)
+                {
+                    int tempNum = breakpoint[0];
+                    bool findNumber = false;
+                    foreach (int bp in breakpoint) 
+                    {
+                        if (bp > currentLine && bp <= tempNum)
+                        {
+                            tempNum = bp;
+                            findNumber = true;
+                        }
+                    }
+                    if (findNumber) 
+                    {
+                        currentLine = tempNum;
+                    }
+                    if (!findNumber)
+                    {
+                        currentLine = length;
+                    }
+                }
+            }
+            return currentLine;
         }
     }
 }
